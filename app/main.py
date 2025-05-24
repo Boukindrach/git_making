b01fc6503d451709c23a6db87a5a06d259d0a1a9
Rachid Boukind <boukind.r1@gmail.com>
2025-05-24 22:05:12 +0100

    cat_file

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 import sys
 import os
+import subprocess
+import zlib
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!", file=sys.stderr)
 
-    # Uncomment this block to pass the first stage
-    #
     command = sys.argv[1]
     if command == "init":
         os.mkdir(".git")
@@ -16,6 +16,23 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+    if command == "cat-file":
+        result = subprocess.run(
+            ['git', 'hash-object', '-w', 'test.txt'],
+            capture_output=True,
+            text=True                 
+        )
+        hash_value = sys.argv[3]
+        with open(f".git/objects/{hash_value[:2]}/{hash_value[2:]}", "rb") as f:
+            compressed = f.read()
+            decompressed = zlib.decompress(compressed)
+        s = decompressed.decode('utf-8')
+        for i in range(5, len(s)):
+            if s[i] >= 'a' and s[i] <= 'z':
+                print(s[i:], end="")
+                return
+
+
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
